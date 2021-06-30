4a6cf56b7ee6730a5fa98b1780d9d3e1fc32f480
francs2812 <emiliofrangella2812@gmail.com>
2021-06-30 19:15:37 +0200

    main completato

--- database.php
+++ database.php
@@ -8,78 +8,70 @@ include __DIR__ . '/partials/function.php';
             "genre" => "<span>Genere</span> : Hip hop",
             "cover" => '<img src="imge/articolo31.jpg" alt="">'
         ],
-
         [
-            "title" => "title",
-            "author" => "author",
-            "year" => "year",
-            "genre" => "genre",
-            "cover" =>  '<img src="imge/articolo31.jpg" alt="">'
+            "title" => "<span>Titolo</span> : New Jersey",
+            "author" => "<span>Autore</span> : Bon Jovi",
+            "year" => " <span>Anno</span> : 1988",
+            "genre" => "<span>Genere</span> : Rock",
+            "cover" => '<img src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg" alt="">'
+        ],
+        [
+            "title" => "<span>Titolo</span> : Chosen",
+            "author" => "<span>Autore</span> : Maneskin",
+            "year" => " <span>Anno</span> : 2017",
+            "genre" => "<span>Genere</span> : Rock",
+            "cover" => '<img src="imge/maneskin.jpg" alt="">'
+        ],
+        [
+            "title" => "<span>Titolo</span> : Live at Wembley 86",
+            "author" => "<span>Autore</span> : Queen",
+            "year" => " <span>Anno</span> : 1992",
+            "genre" => "<span>Genere</span> : Pop",
+            "cover" => '<img src="https://img.discogs.com/vknPDdrqRbT92pNRX0W4I5N91jg=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-1246953-1448927086-6590.jpeg.jpg" alt="">'
+        ],
+        [
+            "title" => "<span>Titolo</span> : Ten's Summoner's Tales",
+            "author" => "<span>Autore</span> : Sting",
+            "year" => " <span>Anno</span> : 1993",
+            "genre" => "<span>Genere</span> : Pop",
+            "cover" => '<img src="https://images-na.ssl-images-amazon.com/images/I/41JD3CW65HL.jpg" alt="">'
+        ],
+        [
+            "title" => "<span>Titolo</span> : Steve Gadd Band",
+            "author" => "<span>Autore</span> : Steve Gadd Band",
+            "year" => " <span>Anno</span> : 2018",
+            "genre" => "<span>Genere</span> : Jazz",
+            "cover" => '<img src="https://cdn2.jazztimes.com/2018/05/SteveGadd-800x723.jpg" alt="">'
+        ],
+        [
+            "title" => "<span>Titolo</span> : Brave new World",
+            "author" => "<span>Autore</span> : Iron Maiden",
+            "year" => " <span>Anno</span> : 2000",
+            "genre" => "<span>Genere</span> : Metal",
+            "cover" => '<img src="https://images-na.ssl-images-amazon.com/images/I/810nSIQOLiL._SY355_.jpg" alt="">'
+        ],
+        [
+            "title" => "<span>Titolo</span> : One more car, one more raider",
+            "author" => "<span>Autore</span> : Eric Clapton",
+            "year" => " <span>Anno</span> : 2002",
+            "genre" => "<span>Genere</span> : Rock",
+            "cover" => '<img src="https://upload.wikimedia.org/wikipedia/en/9/97/Eric_Clapton_OMCOMR.jpg" alt="">'
+        ],
+        [
+            "title" => "<span>Titolo</span> : Made in Japan",
+            "author" => "<span>Autore</span> : Deep Purple",
+            "year" => " <span>Anno</span> : 1972",
+            "genre" => "<span>Genere</span> : Rock",
+            "cover" => '<img src="https://images-na.ssl-images-amazon.com/images/I/51rggtPgmRL.jpg" alt="">'
+        ],
+        [
+            "title" => "<span>Titolo</span> : And Justice for All",
+            "author" => "<span>Autore</span> : Metallica",
+            "year" => " <span>Anno</span> : 1988",
+            "genre" => "<span>Genere</span> : Metal",
+            "cover" => '<img src="https://images-na.ssl-images-amazon.com/images/I/81r3FVfNG3L._SY355_.jpg" alt="">'
         ],
 
-        [
-            "title" => "title",
-            "author" => "author",
-            "year" => "year",
-            "genre" => "genre",
-            "cover" => "cover",
-        ],
-
-        [
-            "title" => "title",
-            "author" => "author",
-            "year" => "year",
-            "genre" => "genre",
-            "cover" => "cover",
-        ],
-
-        [
-            "title" => "title",
-            "author" => "author",
-            "year" => "year",
-            "genre" => "genre",
-            "cover" => "cover",
-        ],
-
-        [
-            "title" => "title",
-            "author" => "author",
-            "year" => "year",
-            "genre" => "genre",
-            "cover" => "cover",
-        ],
-
-        [
-            "title" => "title",
-            "author" => "author",
-            "year" => "year",
-            "genre" => "genre",
-            "cover" => "cover",
-        ],
-
-        [
-            "title" => "title",
-            "author" => "author",
-            "year" => "year",
-            "genre" => "genre",
-            "cover" => "cover",
-        ],
-
-        [
-            "title" => "title",
-            "author" => "author",
-            "year" => "year",
-            "genre" => "genre",
-            "cover" => "cover",
-        ],
-
-        [
-            "title" => "title",
-            "author" => "author",
-            "year" => "year",
-            "genre" => "genre",
-            "cover" => "cover",
-        ]
     ];
 
 ?>
